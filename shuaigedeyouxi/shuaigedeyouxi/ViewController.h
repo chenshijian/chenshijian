2bd921fece0124e1b6c15bb096a637bcb380bc69
斩尘 <chenshijian9304@163.com>
2016-04-06 12:44:41 +0800

    创建帅哥的游戏

---
+++ shuaigedeyouxi/shuaigedeyouxi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shuaigedeyouxi
+//
+//  Created by 斩尘 on 16/4/6.
+//  Copyright © 2016年 chenshuai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ shuaigedeyouxi/shuaigedeyouxi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  shuaigedeyouxi
+//
+//  Created by 斩尘 on 16/4/6.
+//  Copyright © 2016年 chenshuai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSLog(@"帅哥的游戏开始了");
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
